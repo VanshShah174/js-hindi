79b5ce3ee0c3de34883006e68d539e14d93a2d31
VanshShah174 <97951063+VanshShah174@users.noreply.github.com>
2023-11-22 18:33:14 +0000

    datatypes

---
+++ 01_Basics/02_dataTypes.js
@@ -0,0 +1,22 @@
+"use strict"; // treat all JS code as newer version
+
+// alert( 3 + 3 ) // we are using node js not browser
+
+
+
+
+
+
+// number => 2 to power 53
+// bigint
+// string => ""
+// boolean => true / false
+// null => standalone value  (as well as a datatype)
+// undefined => value is not defined
+// symbol => unique
+
+// object
+
+// console.log(typeof "vansh"); // string
+// console.log(typeof undefined) // undefined
+// console.log(typeof null) // object
