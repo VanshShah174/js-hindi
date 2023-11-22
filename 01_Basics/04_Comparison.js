0bfc54d73f43cb2420248ce7efdc60a121ca7d45
VanshShah174 <97951063+VanshShah174@users.noreply.github.com>
2023-11-22 20:06:39 +0000

    comparison done

---
+++ 01_Basics/04_Comparison.js
@@ -0,0 +1,17 @@
+// console.log("2" > 1);
+// console.log("02" > 1)
+
+
+console.log( null > 0);
+console.log( null == 0 );
+console.log( null >= 0);
+
+// here comparisons convert null to a number, treating as a 0. that's why here null(0) > 0 (false) and null >= 0 (true)
+
+console.log(undefined == 0);
+
+
+
+// console.log("1" === 1); // false
+// === checks value and also the datatypes 
+
