6692bb49d2d014fb3387f140ef5163571e0b6983
VanshShah174 <97951063+VanshShah174@users.noreply.github.com>
2023-11-22 19:56:45 +0000

    done till conversion

--- 01_Basics/03_ConversionOperation.js
+++ 01_Basics/03_ConversionOperation.js
@@ -1,11 +1,11 @@
 let score = "vansh"
 
-console.log(typeof score);
-console.log(typeof (score));
+// console.log(typeof score);
+// console.log(typeof (score));
 
 let valueInNumber = Number(score)
-console.log(typeof valueInNumber);
-console.log(valueInNumber)
+// console.log(typeof valueInNumber);
+// console.log(valueInNumber)
 
 
 
@@ -30,5 +30,35 @@ let booleanIsLoggedIn = Boolean(isLoggedIn)
 let someNumber = 33
 
 let stringNumber = String(someNumber)
-console.log(stringNumber);
-console.log(typeof stringNumber)
+// console.log(stringNumber);
+// console.log(typeof stringNumber)
+
+
+// *************************************Operations************************************************* //
+
+let value = 3
+
+let negValue = -value
+// console.log(negValue);
+
+// console.log("1" + 2)  // shift + alt + up arrow to duplicate
+// console.log(1 + "2") 
+// console.log("1" + 2 + 2) 
+// console.log(1 + 2 + "2")  // if the string is first then all will be counted as a string and if that is a number then it will be counted as a number
+// console.log(1 + "2")
+
+
+// console.log(+true);
+// console.log(+"");
+
+let num1,num2,num3
+
+num1 = num2 = num3 = 2 + 2
+
+let gameCounter = 100
+gameCounter++
+console.log(gameCounter);
+
+// link to study 
+// https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Operators/Increment
+
